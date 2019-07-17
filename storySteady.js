11ee5e9dfe4284610ef10cc0200bdbb7de80bd52
Feevah <cddefever@gmail.com>
2019-07-17 15:34:41 -0400

    cleaner

--- storySteady.js
+++ storySteady.js
@@ -7,66 +7,22 @@ window.onload = function(){
 	var newOne =  document.querySelector(".newOne");
 	var newStory = document.querySelector(".newStory")
 	
-
-
-
-    	// var newStory = ["Build App", "Boogie",]
-
 	
 	submit.addEventListener("click", function(anEvent){
 
-		userText.value = ""
-
 		var userItem = document.createElement("p");
-		// var newStory = userText.value;
-		var write = document.createTextNode(newStory);
-		newStory.innerHTML = userText.value;
-		userItem.appendChild(newStory);
 
-		
+		document.getElementById('added').textContent = userItem;
 
-		// for (i=0; i<20; i++) {
-		// 	if 
-		// 	newOne.appendChild(userItem);
+		var newDoDo = userText.value;
+
+		userText.value = "";
+
+		userItem.innerHTML = newDoDo;
+
+		newStory.appendChild(userItem);
+
+
 		});
 
-		
-        // var listItems = [first, second, third] 
-		// userItem.innerHTML = newToDo + clicks
-
-
-
-
-		 // if (clicks = 1){
-			// document.querySelector(".first").innerHTML = newToDo
-   //       	};
-
-
 	};
-
-
-
-//   function update(){
-
-// 	var userItem = document.createElement("li");
-//          	if (clicks = 1){
-//          		newToDo.innerHTML = first
-//          		newOne.appendChild(userItem);
-//          	};
-//          	if(clicks = 2){
-//          		userItem.innerHTML = second
-//          		newOne.appendChild(userItem);
-//          	}
-//          	if(clicks = 3){
-//          		userItem.innerHTML = second
-//          		newOne.appendChild(userItem);
-//          	}
-//             else {
-//          		userItem.innerHTML = newToDo
-// 				newOne.appendChild(userItem)
-//          	}
-
-
-
-		
-// }
